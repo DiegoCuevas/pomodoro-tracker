c351fe6233f725f56ece05cb8b89c4b17594681a
Diego Cuevas <diegocuevas579@gmail.com>
2021-08-31 11:53:58 -0500

    Wip: design

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Clock from "./clock/Clock.js"
 function App() {
   return (
-    <div>
-      <div className="text-xl">Pomodoro Tracker</div>
-      <Clock />
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-1/4 h-2/3 border rounded-2xl border-white p-10">
+        <div className="text-xl font-bold text-center my-5">Pomodoro Tracker</div>
+        <Clock />
+      </div>
     </div>
   );
 }
