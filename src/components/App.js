41cd2aeb166d67d8f8802319973c859edac3d42a
Diego Cuevas <diegocuevas579@gmail.com>
2021-08-30 23:20:26 -0500

    ADD: clock component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Clock from "./clock/Clock.js"
+function App() {
+  return (
+    <div>
+      <div className="text-xl">Pomodoro Tracker</div>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
