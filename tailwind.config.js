02a12298cef3676a68775ba6295a3f00fd2b8929
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-03 20:10:16 -0500

    ADD: shadow white

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    boxShadow: {
+      white: "-7px -6px 20px -8px rgba(255,255,255,0.94);",
+    },
   },
   variants: {
     extend: {},
