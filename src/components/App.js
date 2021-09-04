018d55cbae01de8b7ca2aa1e21806afb1abba7e6
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-03 20:10:26 -0500

    ADD: shadow

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import Clock from "./clock/Clock.js"
 function App() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="w-1/4 h-2/3 border rounded-2xl border-white p-10">
+      <div className="w-1/4 h-2/3 rounded-2xl p-10 shadow-white">
         <div className="text-xl font-bold text-center my-5">Pomodoro Tracker</div>
         <Clock />
       </div>
